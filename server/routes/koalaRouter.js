564ebfad2669c241f9afee4dc59ec35bd23bb876
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-19 15:51:05 -0500

    PUT route created

--- server/routes/koalaRouter.js
+++ server/routes/koalaRouter.js
@@ -100,4 +100,36 @@ router.delete('/:id', function (req, res) {
     }); // END POOL
 });
 
+
+//PUT route
+router.put('/:id', function (req, res) {
+    var koala = req.body;
+    var koalaId = req.params.id; //only getting the id from the client I'm deleting
+    console.log(koala); // Has a name and cost
+    
+    // Attempt to connect to the database
+    pool.connect(function (errorConnectingToDb, db, done) {
+        if (errorConnectingToDb) {
+            // There was an error and no connection was made
+            console.log('Error connecting', errorConnectingToDb);
+            res.sendStatus(500);
+        } else {
+            // We connected to the db!!!!! pool -1
+            //var queryText = 'INSERT INTO "koalas" ("name", "age", "gender", "transfer", "notes") VALUES ($1, $2, $3 , $4 , $5 );';
+            var queryText = 'UPDATE "koalas" SET "name"= $1, "age" = $2, "gender" = $3, "readyForTransfer" = $4, "notes" = $5 WHERE "id" =$6;';
+            db.query(queryText, [koala.name, koala.age, koala.gender, koala.transfer, koala.notes ], function (errorMakingQuery, result) {
+                // We have received an error or result at this point
+                done(); // pool +1
+                if (errorMakingQuery) {
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    // Send back success!
+                    res.sendStatus(201);
+                }
+            }); // END QUERY
+        }
+    }); // END POOL
+});
+
 module.exports = router;
