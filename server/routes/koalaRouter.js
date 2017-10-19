358332463ac3892b3d0dbec7aad3d1280e110422
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-19 13:33:07 -0500

    GET route created

--- server/routes/koalaRouter.js
+++ server/routes/koalaRouter.js
@@ -10,3 +10,31 @@ var config = {
     max: 10,
     idleTimeoutMillis: 30000
 }
+
+//GET route
+router.get('/', function(req, res){
+    //attempt to connect to db
+    pool.connect(function(errorConnectingToDb, db, done){
+        if(errorConnectingToDb) {
+            console.log('Error connecting', errorConnectingToDb);
+            res.sendStatus(500);
+        } else {
+            // We connected to the db, pool - 1
+            var queryText = 'SELECT * FROM "koalas";';
+            db.query(queryText, function(errorMakingQuery, result){
+                //We have recieved an error or result at this point
+                done(); //pool + 1
+                if(errorMakingQuery) {
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                }
+            }); //END QUERY
+        }
+    }); //END POOL
+}); //END GET ROUTE
+
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 5000;
-
+var koalaRouter = require('./routes/koalaRouter.js');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
-
+app.use('/koalaRouter', koalaRouter);
 // Start listening for requests on a specific port
 app.listen(port, function(){
   console.log('listening on port', port);
